9890f461fa1e80064e66e5dc71912e63eb136aad
Arpesh Shrivastava <arpesh_11@hotmail.com>
2024-04-28 23:43:19 +0530

    Added Todo

--- book_directory/Frontend/src/AppProvider.jsx
+++ book_directory/Frontend/src/AppProvider.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import Routes from "./Routes/Routes";
-// import { useGetAllTheBooksQuery } from "./Api/booksApi";
 
+/*
+  TODO: CReate a Search 
+  TODO: Create a SortBy Page
+  TODO: Create a Book Detail Page
+
+*/
 const AppProvider = () => {
   return (
     <Provider store={store}>
